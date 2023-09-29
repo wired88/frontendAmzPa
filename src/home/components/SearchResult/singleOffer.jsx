e66a704950b64f54aabd42c13f2f488e0352056f
wired <wired67@outlook.de>
2023-09-29 03:43:14 +0200

    08

--- src/home/components/SearchResult/singleOffer.jsx
+++ src/home/components/SearchResult/singleOffer.jsx
@@ -1,9 +1,26 @@
 import mainHome from "../../../assets/mainHome.jpg";
 import amzWhiteLogo from "../../../assets/amzWhiteLogo.png";
-import ratingStarDefault from "../../../../public/ratingStarDefault.svg"
 import stars from "../../../assets/stars.png";
+import {Carousel} from "react-bootstrap";
 // eslint-disable-next-line react/prop-types
 function SingleOffer({number}) {
+  const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 1
+        },
+        mobile: {
+            breakpoint: { max: 500, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    };
   return(
     <section className={"productElement bg-dark"}>
       <div className={"productIndex"}>
@@ -15,7 +32,44 @@ function SingleOffer({number}) {
         </p>
       </div>
       <div className={"productImageContainer "}>
-        <img className={"productImage"} src={mainHome} alt="img"/>
+        <div style={{width: "100%", height: "50%", overflow: "hidden"}}>
+            <Carousel
+              swipeable={true}
+              draggable={true}
+              showDots={true}
+              responsive={responsive}
+              ssr={true}
+              infinite={true}
+              autoPlay={document.deviceType !== "mobile"}
+              autoPlaySpeed={2000}
+              keyBoardControl={true}
+              customTransition="all 500ms ease-in-out"
+              transitionDuration={500}
+              containerClass="carousel-container"
+              removeArrowOnDeviceType={["mobile"]}
+              deviceType={document.deviceType}
+              dotListClass="custom-dot-list-style"
+              itemClass="productImage"
+              centerMode={true}
+              style={{width: "100%", height: "100%"}}
+              >
+              <div>
+                  <img className={"productImage"} src={mainHome} alt="img"/>
+              </div>
+
+              <div>
+                  <img className={"productImage"} src={mainHome} alt="img"/>
+              </div>
+
+              <div>
+                  <img className={"productImage"} src={mainHome} alt="img"/>
+              </div>
+
+              <div>
+                  <img className={"productImage"} src={mainHome} alt="img"/>
+             </div>
+            </Carousel>
+          </div>
       </div>
       <div className={"informationContainer bg-dark"}>
         <div className={"ul-container"}>
